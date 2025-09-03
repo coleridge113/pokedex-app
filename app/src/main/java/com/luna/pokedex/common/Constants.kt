08daa9f993a7100bb0c6d0aacdd6ba266a278c22
Jose Luna <josemanuelmluna@gmail.com>
2025-09-03 15:03:24 +0800

    finished part 5

--- app/src/main/java/com/luna/pokedex/common/Constants.kt
+++ app/src/main/java/com/luna/pokedex/common/Constants.kt
@@ -3,4 +3,6 @@ package com.luna.pokedex.common
 object Constants {
 
     const val BASE_URL = "https://pokeapi.co/api/"
+
+    const val PAGE_SIZE = 20
 }
--- app/src/main/java/com/luna/pokedex/presentation/pokemon_list/PokemonListViewModel.kt
+++ app/src/main/java/com/luna/pokedex/presentation/pokemon_list/PokemonListViewModel.kt
@@ -3,19 +3,74 @@ package com.luna.pokedex.presentation.pokemon_list
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asAndroidBitmap
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.palette.graphics.Palette
+import com.luna.pokedex.common.Constants.PAGE_SIZE
+import com.luna.pokedex.common.Resource
+import com.luna.pokedex.domain.model.pokemon_list_entry.PokemonListEntry
 import com.luna.pokedex.domain.repository.Repository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import java.util.Locale
 import javax.inject.Inject
 
 @HiltViewModel
 class PokemonListViewModel @Inject constructor(
     private val repository: Repository
 ) : ViewModel() {
+
+    private var curPage = 0
+    var pokemonList = mutableStateOf<List<PokemonListEntry>>(listOf())
+    var loadError = mutableStateOf("")
+    var isLoading = mutableStateOf(false)
+    var endReached = mutableStateOf(false)
+
+    init {
+        loadPokemonPaginated()
+    }
+
+    fun loadPokemonPaginated() {
+        viewModelScope.launch {
+            isLoading.value = true
+            val result = repository.getPokemonList(
+                limit = PAGE_SIZE,
+                offset = curPage * PAGE_SIZE
+            )
+            when(result) {
+                is Resource.Success -> {
+                    endReached.value = curPage * PAGE_SIZE >= result.data!!.count
+                    val pokedexEntries = result.data.results.mapIndexed { index, entry ->
+                        val number = if (entry.url.endsWith("/")) {
+                            entry.url.dropLast(1).takeLastWhile { it.isDigit() }
+                        } else {
+                            entry.url.takeLastWhile { it.isDigit() }
+                        }
+                        val url =
+                            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${number}.png"
+                        PokemonListEntry(
+                            entry.name.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.ROOT) else it.toString() },
+                            url,
+                            number.toInt()
+                        )
+                    }
+                    curPage++
+                    loadError.value = ""
+                    isLoading.value = false
+                    pokemonList.value += pokedexEntries
+                }
+
+                is Resource.Error -> {
+                    loadError.value = result.message!!
+                    isLoading.value = false
+                }
+            }
+        }
+    }
 
     fun calcDominantColor(
         image: ImageBitmap,
