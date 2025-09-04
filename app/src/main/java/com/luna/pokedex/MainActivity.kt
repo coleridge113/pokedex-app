53e1be4bb62c840a26d973cac4c964ef751b5f00
Jose Luna <josemanuelmluna@gmail.com>
2025-09-04 10:46:54 +0800

    cleaned up code

--- app/src/main/java/com/luna/pokedex/MainActivity.kt
+++ app/src/main/java/com/luna/pokedex/MainActivity.kt
@@ -3,14 +3,8 @@ package com.luna.pokedex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -26,7 +20,6 @@ import java.util.Locale
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // enableEdgeToEdge()
         setContent {
             PokedexTheme {
                 val navController = rememberNavController()
